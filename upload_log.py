2cf6a8c3cb8934b262440b19bd14755951c0e169
vath414 <dydyloy69@gmail.com>
2025-10-29 10:41:25 +0700

    Update upload_log.py

--- upload_log.py
+++ upload_log.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-upload =[]
-=======
-upload = ['Aaand_we_re_back_this_time_with_FOLDING_THE_EGGS_IN_viralbreak....mp4', 'And_it_has_70g_of_protein_I_m_obsessed_highprotein_cottagechee....mp4']
->>>>>>> ec7666db920bd7cc6c097332c5016153de25425a
+upload=[]
